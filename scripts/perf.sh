0991d56cdfee4a1bc90f6ad4c62323799301e9bc
Maneren <49210777+Maneren@users.noreply.github.com>
2022-05-27 15:38:49 +0200

    remove useless scripts

--- scripts/perf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cargo build --release
-
-/usr/lib/linux-tools/5.4.0-81-generic/perf record  -m 10000 --call-graph=dwarf ./target/release/gomoku "$@"
-/usr/lib/linux-tools/5.4.0-81-generic/perf report --hierarchy -M intel
